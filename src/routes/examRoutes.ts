a3c9492fd75d8ec3901626081910d1bcd3c5b3ec
Randriambahiny Ahitantsoa Delval <ahitantsoa.d@gmail.com>
2026-08-27 15:42:30 +0300

    feat: ajoute la route GET /api/exams/:id/results

--- src/routes/examRoutes.ts
+++ src/routes/examRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ExamController } from "../Controller/ExamController";
 import { QuestionController } from "../Controller/QuestionController";
+import { ResultController } from "../Controller/ResultController";
 import { requireAuth } from "../middlewares/auth";
 import { requireRole } from "../middlewares/requireRole";
 
@@ -21,5 +22,8 @@ router.delete("/:id", ExamController.remove);
 // --- Routes imbriquées /exams/:id/questions ---
 router.get("/:id/questions", QuestionController.findAllByExam);
 router.post("/:id/questions", QuestionController.create);
+
+//Route imbriquée /exams/:id/results (vue admin) ---
+router.get("/:id/results", ResultController.getExamResults);
 
 export default router;
